0ae798a2d08ec1d748391c6b831cba79a17642b8
Pieter Boots <Pieterboots@zonnet.nl>
2025-01-26 15:19:19 +0100

    Initial upload
    A project from a while ago.

---
+++ DrawingBoard.cs
@@ -0,0 +1,262 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace DigitRecognition
+{
+
+    public delegate void NewNumberEvent(object sender, System.Collections.ArrayList lst);
+    public partial class DrawingBoard : UserControl
+    {
+         public event NewNumberEvent NewNumber;
+
+        public enum DrawingStatus
+        {
+            drNone,
+            drDrawing,
+            drWaiting,
+            drFinished,
+        }
+
+        public DrawingStatus fDrawingStatus = DrawingStatus.drNone;
+        List<int[]> lst = new List<int[]>();
+        List<Point> Dots = new List<Point>();
+        List<Point> FoundNumber = new List<Point>();
+
+
+        public DrawingBoard()
+        {
+            InitializeComponent();
+
+            pictureBox1.Image = new Bitmap(480, 640);
+
+            using (Graphics graphics = Graphics.FromImage(pictureBox1.Image))
+            using (System.Drawing.SolidBrush myBrush = new System.Drawing.SolidBrush(System.Drawing.Color.White))
+                graphics.FillRectangle(myBrush, new Rectangle(0, 0, 480, 640));
+
+            clear();
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (fDrawingStatus == DrawingStatus.drFinished)
+                clear();
+
+            fDrawingStatus = DrawingStatus.drDrawing;
+            timer1.Enabled = false;
+        }
+
+        public void ClearDots()
+        {
+            Bitmap bmp = (Bitmap)pictureBox1.Image;
+            foreach (Point aDot in Dots)
+            {
+                DrawDot(bmp, aDot, Color.White);            
+            }
+
+            foreach (Point aDot in FoundNumber)
+            {
+                DrawDot(bmp, aDot, Color.White);
+            }
+        }
+
+        private void clear()
+        {
+            Bitmap bmp = (Bitmap)pictureBox1.Image;
+            ClearDots();
+
+            Dots.Clear();
+            pictureBox1.Image = bmp;
+            pictureBox1.Refresh();
+        }
+
+        public void DrawDot(Bitmap bmp, Point pnt,Color cl)
+        {
+            for (int x = -10; x < 10; x++)
+                for (int y = -10; y < 10; y++)
+                {
+                    if (Math.Sqrt(x * x + y * y) < 10)
+                        if (x + pnt.X >= 0 && x + pnt.X < bmp.Width &&
+                         y + pnt.Y >= 0 && y + pnt.Y < bmp.Height)
+                            bmp.SetPixel(x + pnt.X, y + pnt.Y, cl);
+                }
+        }
+
+
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Bitmap bmp = (Bitmap)pictureBox1.Image;
+
+            if (fDrawingStatus == DrawingStatus.drDrawing)
+                if (bmp != null)
+                {
+                    timer1.Enabled = false;
+                    Dots.Add(new Point(e.X, e.Y));
+                    DrawDot(bmp, new Point(e.X,e.Y), Color.FromArgb(255, 0, 0, 0));                 
+                }
+
+            pictureBox1.Refresh();
+        }
+
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            fDrawingStatus = DrawingStatus.drWaiting;
+            timer1.Interval = 1000;
+            timer1.Enabled = true;
+            timer1.Start();
+        }
+
+        public void DrawFoundNumber(List<Point> aFoundNumber)
+        {
+            if (aFoundNumber == null)
+                return;
+
+            FoundNumber = aFoundNumber;
+            Bitmap bmp = (Bitmap)pictureBox1.Image;
+            for (int i = 0; i < aFoundNumber.Count; i++)
+            {
+                Point pnt = aFoundNumber[i];
+                DrawDot(bmp, pnt, Color.FromArgb(255, 0, 255, 0));
+            }
+            pictureBox1.Refresh();
+        }
+
+
+        public void GetSpecsDots(List<Point> Dots, ref double scale, ref Point center)
+        {
+            Bitmap bmp = (Bitmap)pictureBox1.Image;
+            double left = 9999;
+            double right = 0;
+            double top = 9999;
+            double bottom = 0;
+            double xtot = 0;
+            double ytot = 0;
+
+            if (Dots.Count == 0)
+                return;
+            foreach (Point aDot in Dots)
+            {
+                if (aDot.X > right)
+                    right = aDot.X;
+                if (aDot.X < left)
+                    left = aDot.X;
+                if (aDot.Y > bottom)
+                    bottom = aDot.Y;
+                if (aDot.Y < top)
+                    top = aDot.Y;
+
+                xtot = xtot + aDot.X;
+                ytot = ytot + aDot.Y;
+            }
+
+            xtot = xtot / Dots.Count;
+            ytot = ytot / Dots.Count;
+
+            int W = System.Convert.ToInt16(right - left);
+            int H = System.Convert.ToInt16(bottom - top);
+
+            int pick = 0;
+            for (int i = 0; i < 1000; i++)
+            {
+                double nW = W * (0.5 + i / 100.0);
+                double nH = H * (0.5 + i / 100.0);
+                if ((nW > 300) || (nH > 400))
+                {
+                    pick = i;
+                    break;
+                }
+            }
+
+            scale = (0.5 + pick / 100.0);
+            center = new Point(System.Convert.ToInt16(xtot), System.Convert.ToInt16(ytot));
+
+            ClearDots();
+
+            for (int i = 0; i < Dots.Count; i++)
+            {
+                int x =System.Convert.ToInt32( (Dots[i].X - xtot) * scale);
+                int y = System.Convert.ToInt32((Dots[i].Y - ytot) * scale);
+                Dots[i] = new Point(x + bmp.Width / 2, y + bmp.Height / 2);
+            }            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            fDrawingStatus = DrawingStatus.drFinished;
+
+            if (Dots.Count == 0)
+                return;
+
+            double Scale = 1.0;
+            Point Center = new Point(0, 0);
+            GetSpecsDots(Dots, ref Scale, ref Center);
+
+            Bitmap bmp = (Bitmap)pictureBox1.Image;
+            double pick = 0;
+            Point prev = new Point(0, 0);
+
+            System.Collections.ArrayList Dots50 = new System.Collections.ArrayList();
+            Point pnt = new Point(0, 0);
+
+            for (int p = 0; p < 400; p++)
+            {
+                int tot = 0;
+                prev = new Point(0, 0);
+                for (int i = 0; i < Dots.Count; i++)
+                {
+                    pnt = Dots[i];
+                    double d = Math.Sqrt((pnt.X - prev.X) * (pnt.X - prev.X) +
+                        (pnt.Y - prev.Y) * (pnt.Y - prev.Y));
+                    if (d > p / 4)
+                    {
+                        tot++;
+                        prev = pnt;
+                    }
+                }
+                if (tot < 51)
+                {
+                    pick = p;
+                    break;
+                }
+            }
+
+            prev = new Point(0, 0);
+            for (int i = 0; i < Dots.Count; i++)
+            {
+                pnt = Dots[i];
+                double d = Math.Sqrt((pnt.X - prev.X) * (pnt.X - prev.X) +
+                    (pnt.Y - prev.Y) * (pnt.Y - prev.Y));
+
+                if (d > pick / 4)
+                {
+                    Dots50.Add(pnt);                  
+                    if (bmp != null)
+                        DrawDot(bmp, pnt, Color.FromArgb(255, 255, 0, 0));
+                    prev = pnt;
+                }
+            }
+
+            Dots50.Add(Dots[Dots.Count - 1]);
+            pnt = Dots[Dots.Count - 1];
+            if (bmp != null)
+                DrawDot(bmp, pnt, Color.FromArgb(255, 255, 0, 0));
+
+            pictureBox1.Refresh();
+            if (this.NewNumber != null)
+                NewNumber(this, Dots50);
+            //  Bitmap bmp = (Bitmap)pictureBox1.Image;
+        }
+    }
+    
+}
---
+++ Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace DigitRecognition
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.drawingBoard1 = new DigitRecognition.DrawingBoard();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(554, 108);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(235, 537);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.WordWrap = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(848, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(39, 38);
+            this.textBox1.TabIndex = 10;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(848, 108);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox3.Size = new System.Drawing.Size(723, 537);
+            this.textBox3.TabIndex = 11;
+            this.textBox3.WordWrap = false;
+            // 
+            // drawingBoard1
+            // 
+            this.drawingBoard1.Location = new System.Drawing.Point(12, 28);
+            this.drawingBoard1.Name = "drawingBoard1";
+            this.drawingBoard1.Size = new System.Drawing.Size(513, 645);
+            this.drawingBoard1.TabIndex = 9;
+            this.drawingBoard1.NewNumber += new DigitRecognition.NewNumberEvent(this.drawingBoard1_NewNumber);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(560, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 31);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1617, 701);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.drawingBoard1);
+            this.Controls.Add(this.textBox2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox textBox2;
+        private DrawingBoard drawingBoard1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
